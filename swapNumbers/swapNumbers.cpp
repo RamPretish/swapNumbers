5ab40f8f65b1129d80c3ba8b4136f27d294b160f
RamPretish <81359843+RamPretish@users.noreply.github.com>
2024-07-12 20:49:48 +0200

    - modified from pointers to reference

--- swapNumbers/swapNumbers.cpp
+++ swapNumbers/swapNumbers.cpp
@@ -5,17 +5,17 @@
 
 using namespace std;
 
-void swapNumber(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+void swapNumber(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
 }
 
 int main()
 {
 	int a = 10;
 	int b = 20;
-	swapNumber(&a, &b);
+	swapNumber(a, b);
 	cout << a << '\t' << b << endl;
 	return 0;
 }
