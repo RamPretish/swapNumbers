80d26a828fd197f3d5d009b407af34415552589c
RamPretish <81359843+RamPretish@users.noreply.github.com>
2024-07-12 19:14:06 +0200

    - added swapNumbers code - added build files - added CMakeLists

---
+++ swapNumbers/swapNumbers.cpp
@@ -0,0 +1,21 @@
+﻿// swapNumbers.cpp : Defines the entry point for the application.
+//
+
+#include "swapNumbers.h"
+
+using namespace std;
+
+void swapNumber(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	swapNumber(&a, &b);
+	cout << a << '\t' << b << endl;
+	return 0;
+}
